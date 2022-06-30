53060ee969d6fddefcb3343def7cf0d0425d26dc
Nicolas Firmo <nicolasfpbarra@gmail.com>
2022-06-30 19:52:58 -0300

    Added text
    Introducing MesloLGS as the app base font.

--- app/jni/src/main.cpp
+++ app/jni/src/main.cpp
@@ -27,6 +27,27 @@ int main(int /*argc*/, char * /*argv*/[]) {
 	SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, loadedImage);
 	SDL_FreeSurface(loadedImage);
 
+	if (TTF_Init() == -1) {
+		SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "TTF_Init: %s\n",
+					 TTF_GetError());
+		return 1;
+	}
+
+	TTF_Font *font = TTF_OpenFont("fonts/MesloLGS NF Regular.ttf", 64);
+	if (!font) {
+		SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Unable to load font:%s\n",
+					 TTF_GetError());
+		return 1;
+	}
+
+	SDL_Color textColor = {.r = 0xff, .g = 0xff, .b = 0xff, .a = 0xff};
+	SDL_Surface *solid =
+		TTF_RenderText_Solid(font, "MetroTuner Initialized", textColor);
+	SDL_Rect topRect{.w = solid->w, .h = solid->h};
+
+	SDL_Texture *solidTexture = SDL_CreateTextureFromSurface(renderer, solid);
+	SDL_FreeSurface(solid);
+
 	SDL_Rect centerRect{.w = 512, .h = 512};
 	centerRect.x = (width - centerRect.w) / 2;
 	centerRect.y = (height - centerRect.h) / 2;
@@ -49,6 +70,7 @@ int main(int /*argc*/, char * /*argv*/[]) {
 		}
 		}
 		SDL_RenderCopy(renderer, texture, nullptr, &centerRect);
+		SDL_RenderCopy(renderer, solidTexture, nullptr, &topRect);
 		SDL_RenderPresent(renderer);
 	}
 
